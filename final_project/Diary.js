cf837b5365a5778f519ef018a099106849662b95
sahil2019 <sahil2011.raj@gmail.com>
2021-01-01 11:40:09 +0530

    sahil:v2

--- final_project/Diary.js
+++ final_project/Diary.js
@@ -1,25 +1,34 @@
- let html=""
- const image=["data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAbCAYAAABm409WAAABhElEQVR42rVWTU6DQBSeI/QIPUJvIEln0pmYaBcuFExkI7Czexd2b6IcgbVg6E53kniAdg8Eb9DeoL6HTENbsFpmXvLCMMD3vf+BkAZxQmviRtaHVCe6Gstndmz34PmzE5pz0IQcI25ozgB4XVfv1bS9+LLvRmYh944m8MJrY5fAiaxl5ZEEXwGZT46VKkzrFl1gqEhXuX0xB2DpFMnAg6Aerk7ABWODdETPMz68wfVuXtD6wjB6hWAn+Yjd4RXvDwJnYjhOBZtngq23lLPi3Tl9e3y6mAf3Z5+ZoPHeO4Iuc04fWolyQSf7H/1f0cA9ErRcBXidZJuA05VKAlSMSAXObNXgMm/S+pkWAtCyAlPOFroIMLdEHzgkm9OpVoIy0dAgXxo9MAjUbKKVABa+LgItXVzr5kRrJ2MD12aR4jCBwVsDr+BGX7EHfsNZoMiLXes3XsCmip7YTNEmwbrtaP3s8LH56/hu9xCH5p/OZemJnLJlp9dLrvopgL3gp7xLUr8N/BsO/opsSCT2igAAAABJRU5ErkJggg==",
-"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABoAAAAbCAYAAABiFp9rAAABiklEQVR42rVW0U3DMBDNCIzACGxAR3A26AawAd2AfNcuzQZ0A9igbEDli8RnuwG9s5Nix2fjGmPJaqsqfnfvvXtO0/xhie3XrVDwKpQ+4v7E7w9N7SW2xxshYd8q+Ha3kMNjXSAJizmIBYJ9XSClBQeE+1BMUfsC99N29WmlPgUdKeiKhGbpIQMgdaYrF0zqHRX2o6F+nn7zIJthGaElBKSCCBQ/vSJHoxg3roe7PBAJ73aHVF0AN/Bk9mh37388MwAiMTkqRqetcjp1nj+xIN6B2EGomV9pmlbdu3RmL+oucujHRSccVqtXwgAlwzkO6KLacHq6+RrsakbNKip0iQbxoeUNQAXUzLM+lmdFgpPX5zSkDGA7+iVeAhCMB+5hsq1rYc4Q9sLLvOxMtIQPe2E6ZZUdZCaxc/QaKTokgrNjblYP0BSXSyNNNVNtVHRzT/lg/XXvAdhB7tTPZ+zqlDDzk0rdSGpgkW/Nf635u0NugWVZ6DjX3Kil6Z1DtZeBNRM9pBA6syPdnAHd7Oe2GWnwrAAAAABJRU5ErkJggg==",
-"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABcAAAAbCAYAAACX6BTbAAABE0lEQVR42mPwXrpU0HPh4nleixa/A+L/FOOFi295LlpUzAACngsXzaWKoWjYY9GiIAZSXBy6fAXRhgMdvQJk+B98ipr2H/h/9927/8jg2KNH/xPWriNg+OI9eA3ffefuf3Tw5dcvMP3550+wxWQZDtKIzeC+o0fhfJAFuHyA1/CXX75gGJyzecv/8p27UMRBviPJcJAh6AAWBOiGv/j8hTTDQV5devEiHCOHLbocCOM2fOGiv7RI5/QzPGrV6v+nHj+hCO+8fRu74Ynr1v2nFDz//JmOhnsuWvxv1PBhZDiMM2o4VsNjVq/5f+XlS4rwofsPsBtOq4LrPY0MX80AbRDRot0SyuC+apWQ16JFi2jR4gIAV8p1DXqbIUkAAAAASUVORK5CYII=",
-"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABsAAAAbCAYAAACN1PRVAAAA/UlEQVR42s3WTwqCQBQGcNs3QmeoO7TvAkFnaRUEbTKL1h3EceUqaFFXmBatCqO9jozpNIOEEJraPM0HH/hn8YPhE5/GudahdnfuWejuY53/GmqjmVY0HkaGClIJFJirAgTHMfftXvrMQtNcTLyMVLDwsuNP1yoHQmBySoFQWCkQEisEobGvoI9RDI3lgnVhWaAyFpwmPCSr3ASHERxWJf/DGDE5O2/AEhLjE0tvwCeiLcGYaA8ja7CEZJmPNVCQP2FJi7bKoc4gA0NxLQWh+2HLMPkRJo1UC3X6xVi9EZjqvlg6FrrJP7XZBOZhfaHJjVheiP3xUdPxXd9rwQvV3Qwxk6K2XAAAAABJRU5ErkJggg=="]
-let addFood=()=>{
- html+= `<tr>
- <td><img src=${image[0]}></img></td>
- <td>BANANA</td>
- <td class="add__food__margin"><p>1 medium - 7 7/8" long</p></td>
- <td class="add__food__margin"><p>105.02</p></td>
- </tr>`
- document.querySelector('#home__app__info__add table').innerHTML=html
-}
-let addExercise=()=>{
-    html+= `<tr>
-    <td><img src=${image[1]}></img></td>
-    <td>Walking for pleasure</td>
-    <td class="add__food__margin"><p>30</p></td>
-    <td class="add__food__margin"><p>-89.3</p></td>
-    </tr>`
-    document.querySelector('#home__app__info__add table').innerHTML=html
-   }
-  
-   
+//  let html=""
+//  const image=["data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAbCAYAAABm409WAAABhElEQVR42rVWTU6DQBSeI/QIPUJvIEln0pmYaBcuFExkI7Czexd2b6IcgbVg6E53kniAdg8Eb9DeoL6HTENbsFpmXvLCMMD3vf+BkAZxQmviRtaHVCe6Gstndmz34PmzE5pz0IQcI25ozgB4XVfv1bS9+LLvRmYh944m8MJrY5fAiaxl5ZEEXwGZT46VKkzrFl1gqEhXuX0xB2DpFMnAg6Aerk7ABWODdETPMz68wfVuXtD6wjB6hWAn+Yjd4RXvDwJnYjhOBZtngq23lLPi3Tl9e3y6mAf3Z5+ZoPHeO4Iuc04fWolyQSf7H/1f0cA9ErRcBXidZJuA05VKAlSMSAXObNXgMm/S+pkWAtCyAlPOFroIMLdEHzgkm9OpVoIy0dAgXxo9MAjUbKKVABa+LgItXVzr5kRrJ2MD12aR4jCBwVsDr+BGX7EHfsNZoMiLXes3XsCmip7YTNEmwbrtaP3s8LH56/hu9xCH5p/OZemJnLJlp9dLrvopgL3gp7xLUr8N/BsO/opsSCT2igAAAABJRU5ErkJggg==",
+// "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABoAAAAbCAYAAABiFp9rAAABiklEQVR42rVW0U3DMBDNCIzACGxAR3A26AawAd2AfNcuzQZ0A9igbEDli8RnuwG9s5Nix2fjGmPJaqsqfnfvvXtO0/xhie3XrVDwKpQ+4v7E7w9N7SW2xxshYd8q+Ha3kMNjXSAJizmIBYJ9XSClBQeE+1BMUfsC99N29WmlPgUdKeiKhGbpIQMgdaYrF0zqHRX2o6F+nn7zIJthGaElBKSCCBQ/vSJHoxg3roe7PBAJ73aHVF0AN/Bk9mh37388MwAiMTkqRqetcjp1nj+xIN6B2EGomV9pmlbdu3RmL+oucujHRSccVqtXwgAlwzkO6KLacHq6+RrsakbNKip0iQbxoeUNQAXUzLM+lmdFgpPX5zSkDGA7+iVeAhCMB+5hsq1rYc4Q9sLLvOxMtIQPe2E6ZZUdZCaxc/QaKTokgrNjblYP0BSXSyNNNVNtVHRzT/lg/XXvAdhB7tTPZ+zqlDDzk0rdSGpgkW/Nf635u0NugWVZ6DjX3Kil6Z1DtZeBNRM9pBA6syPdnAHd7Oe2GWnwrAAAAABJRU5ErkJggg==",
+// "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABcAAAAbCAYAAACX6BTbAAABE0lEQVR42mPwXrpU0HPh4nleixa/A+L/FOOFi295LlpUzAACngsXzaWKoWjYY9GiIAZSXBy6fAXRhgMdvQJk+B98ipr2H/h/9927/8jg2KNH/xPWriNg+OI9eA3ffefuf3Tw5dcvMP3550+wxWQZDtKIzeC+o0fhfJAFuHyA1/CXX75gGJyzecv/8p27UMRBviPJcJAh6AAWBOiGv/j8hTTDQV5devEiHCOHLbocCOM2fOGiv7RI5/QzPGrV6v+nHj+hCO+8fRu74Ynr1v2nFDz//JmOhnsuWvxv1PBhZDiMM2o4VsNjVq/5f+XlS4rwofsPsBtOq4LrPY0MX80AbRDRot0SyuC+apWQ16JFi2jR4gIAV8p1DXqbIUkAAAAASUVORK5CYII=",
+// "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABsAAAAbCAYAAACN1PRVAAAA/UlEQVR42s3WTwqCQBQGcNs3QmeoO7TvAkFnaRUEbTKL1h3EceUqaFFXmBatCqO9jozpNIOEEJraPM0HH/hn8YPhE5/GudahdnfuWejuY53/GmqjmVY0HkaGClIJFJirAgTHMfftXvrMQtNcTLyMVLDwsuNP1yoHQmBySoFQWCkQEisEobGvoI9RDI3lgnVhWaAyFpwmPCSr3ASHERxWJf/DGDE5O2/AEhLjE0tvwCeiLcGYaA8ja7CEZJmPNVCQP2FJi7bKoc4gA0NxLQWh+2HLMPkRJo1UC3X6xVi9EZjqvlg6FrrJP7XZBOZhfaHJjVheiP3xUdPxXd9rwQvV3Qwxk6K2XAAAAABJRU5ErkJggg=="]
+// function addFood(){
+//  html+= `<tr>
+//  <td><img src=${image[0]}></img></td>
+//  <td>BANANA</td>
+//  <td class="add__food__margin"><p>1 medium - 7 7/8" long</p></td>
+//  <td class="add__food__margin"><p>105.02</p></td>
+//  </tr>`
+//  document.querySelector('#home__table__info').innerHTML=html
+// }
+// document.getElementById('add__food__button').addEventListener('click',addFood)
+// // let addExercise=()=>{
+// //     html+= `<tr>
+// //     <td><img src=${image[1]}></img></td>
+// //     <td>Walking for pleasure</td>
+// //     <td class="add__food__margin"><p>30 minutes</p></td>
+// //     <td class="add__food__margin"><p>-89.3</p></td>
+// //     </tr>`
+// //     document.querySelector('.home__table__info').innerHTML=html
+// //    }
+// //    document.getElementById('add__exercise__button').addEventListener('click',addExercise)
+// // let addBiometric=()=>{
+// //     html+=`<tr>
+// //     <td><img src=${image[2]}></img></td> 
+// //     <td class="add__food__margin"><p>34 cm</p></td>
+    
+// //     <tr>`
+// //     document.querySelector('.home__table__info').innerHTML=html
+// // }
+// // document.getElementById('add__biometric__button').addEventListener('click',addBiometric)
--- final_project/tables.js
+++ final_project/tables.js
@@ -2,23 +2,107 @@ const arrLeft=[
     {General:[["Energy","kcal","0%"],["Alcohol","g", "No Target"],["caffein","mg","No Target"],["water","g","0%"]]},
     {Carbohydrates:[["Carbs","g","0%"],["Fiber",'g','0%'],["Starch",'g',"No Target"],["Sugar",'g',"No Target"],["Net Carbs","g","0%"]]},
     {Lipids:[["Fats","g","0%"],["monounsaturated",'g',"No Target"],["polysaturated",'g',"No Target"],['omega-6',"g","0%"],["omega-3",'g',"0%"],["saturated","g","n/a"],["TransFat","g","n/a"],["cholestrol","mg","No Target"]]},
-    {Protiens:[["protien","g","0%"],["cystine","g","0%"],["histidine","g","0%"],["isoleucein","g","0%"],["leucine","g","0%"],["Lysine","g","0%"],["Methyonine","g","0%"],["Phenylalanine","g","0%"],["Threonine","g","0%"],["tryptophan","g","0%"],["Tyrosine","g","0%"],["Valine","g","0%"]]},
+    {Protiens:[["protien","g","0%"],["cystine","g","No Target"],["histidine","g","0%"],["isoleucein","g","0%"],["leucine","g","0%"],["Lysine","g","0%"],["Methyonine","g","0%"],["Phenylalanine","g","0%"],["Threonine","g","0%"],["tryptophan","g","0%"],["Tyrosine","g","0%"],["Valine","g","0%"]]},
 ]
 const arrRight=[
-    {Vitamins:[["B1(Thiamine)","mg","0%"],["B12(Cobalmin)","mg","0%"],["Folate","mg","0%"],["Vitamin A","mg","0%"],["Vitamin C","mg","0%"],["Vitamin D","mg","0%"],["Vitamin E","mg","0%"],
+    {Vitamins:[["B1(Thiamine)","mg","0%"],["B12(Cobalmin)","mg","No Target"],["Folate","mg","0%"],["Vitamin A","mg","0%"],["Vitamin C","mg","0%"],["Vitamin D","mg","0%"],["Vitamin E","mg","0%"],
     ["B2(Riboflavin)","mg","0%"],["B3(Niacine)","mg","0%"],["B5(pantothenicAcid)","mg","0%"],["B6(Pyridoxine)","mg","0%"],["Vitamin K","mg","0%"]]},
-    {Minerals:[["calcium","mg","0%"],["copper","mg","0%"],["Iron","mg","0%"],["calcium","mg","0%"],["Magnesium","mg","0%"],
+    {Minerals:[["calcium","mg","0%"],["copper","mg","No target"],["Iron","mg","0%"],["calcium","mg","0%"],["Magnesium","mg","0%"],
     ["Manganese","mg","0%"],["Phosphorous","mg","0%"],["calcium","mg","0%"],["calcium","mg","0%"],]}
 
 ]
+var i=0;
+var j=0;
 function loadData()
 {
     html=""
-    arrLeft.map((ele,i)=>{
-        html+=`<table>
-        <tr>
-            <td></td>
-        </tr>      
-        </table>`
-    })
+    for(i in arrLeft)
+    {
+        var inter=""
+        for(j in arrLeft[i])
+        {
+            
+            inter+=`<tr><th colspan="3" class="table__upper__part">${j}</th></tr>`
+            var data=arrLeft[i][j]
+            for(row=0;row<data.length;row++)
+            {
+                var innerRows=""
+                for(col=0;col<data[row].length;col++)
+                {
+                    if(col==2)
+                    {
+                        innerRows+=`
+                    <td ><div class="last__col__border">${data[row][col]}</div></td>        
+                    `
+                    }
+                    else{
+                        innerRows+=`
+                    <td>${data[row][col]}</td>        
+                    `
+                    }
+                    
+                }
+                if(row%2==1)
+                {
+                    inter+=`<tr bgcolor="#ddd">${innerRows}</tr>`
+                }
+                else{
+                    inter+=`<tr>${innerRows}</tr>`
+                }
+               
+            }
+            html+=`<table class="indiv__tables">${inter}</table>`
+        }
+    }
+    document.getElementById('table__left__div').innerHTML=html
+    i++
+}
+function loadData2()
+{
+    html=""
+    for(i in arrRight)
+    {
+        var inter=""
+        for(j in arrRight[i])
+        {
+            
+            inter+=`<tr><th colspan="3" class="table__upper__part">${j}</th></tr>`
+            var data=arrRight[i][j]
+            for(row=0;row<data.length;row++)
+            {
+                var innerRows=""
+                for(col=0;col<data[row].length;col++)
+                {
+                    if(col==2)
+                    {
+                        innerRows+=`
+                    <td ><div class="last__col__border">${data[row][col]}</div></td>        
+                    `
+                    }
+                    else{
+                        innerRows+=`
+                    <td>${data[row][col]}</td>        
+                    `
+                    }
+                    
+                }
+                if(row%2==1)
+                {
+                    inter+=`<tr bgcolor="#ddd">${innerRows}</tr>`
+                }
+                else{
+                    inter+=`<tr>${innerRows}</tr>`
+                }
+               
+            }
+            html+=`<table class="indiv__tables">${inter}</table>`
+        }
+    }
+    document.getElementById('table__right__div').innerHTML=html
+    j++
+}
+if(i==0&&j==0)
+{
+loadData()
+loadData2()
 }
