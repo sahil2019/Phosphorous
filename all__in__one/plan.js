859e50129a3c92aae6be527df5e5281c49454db3
pavan <pavan.kgoud@gmail.com>
2021-01-01 21:21:00 +0530

    final of cronometer

--- all__in__one/plan.js
+++ all__in__one/plan.js
@@ -9,3 +9,26 @@ document.getElementById("dropdown__img").addEventListener("mouseleave", () =>{
     document.getElementById("img__card").style.visibility = "hidden";
     
 })
+
+
+document.getElementById("popup").addEventListener("click",() => {
+
+    document.getElementById("hidebox").style.display = "block";
+
+})
+
+
+document.getElementById("input1").addEventListener("click",() => {
+
+    document.getElementById("annual").style.display = "block";
+    document.getElementById("monthly").style.display = "none";
+
+})
+
+
+document.getElementById("input2").addEventListener("click",() => {
+
+    document.getElementById("monthly").style.display = "block";
+    document.getElementById("annual").style.display = "none";
+
+})
--- all__in__one/trends.js
+++ all__in__one/trends.js
@@ -9,3 +9,21 @@ document.getElementById("dropdown__img").addEventListener("mouseleave", () =>{
     document.getElementById("img__card").style.visibility = "hidden";
     
 })
+
+
+// onimage
+
+
+// popupdrop
+
+document.getElementById("onimage").addEventListener("click",() => {
+
+    document.getElementById("popupdrop").style.visibility = "visible";
+
+})
+
+
+document.getElementById("onimage").addEventListener("mouseleave", () =>{
+    document.getElementById("popupdrop").style.visibility = "hidden";
+    
+})
